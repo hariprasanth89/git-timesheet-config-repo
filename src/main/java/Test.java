e10bd3bf722eb47889ad93a69789e1eddf461c61
hariprasanth <hariprasanth89@gmail.com>
2023-08-22 11:04:59 -0700

    inital commit config server

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.nio.file.Files;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("user.home"));
+//        String str = '${user.dir}';
+//        System.out.println(str);
+
+
+    }
+}
